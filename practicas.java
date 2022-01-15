3b310718c9b6c1f65a5c4cc9436ac157076be57e
Eliezer <ferrari_italia1997@hotmail.com>
2022-01-15 20:59:31 +0100

    Añadi el boton de salir

--- practicas.java
+++ practicas.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 import java.awt.event.*;
 public class practicas extends JFrame implements ActionListener{
 
-private JButton boton1;
+private JButton boton1, boton2;
+
 		//este es el constructor
 		public practicas(){
 			setLayout(null);
@@ -11,11 +12,20 @@ private JButton boton1;
 			boton1.setBounds(150,150,100,30);
 			add(boton1);
 			boton1.addActionListener(this);
+
+			boton2 = new JButton("Salir");
+			boton2.setBounds(100,100,100,30);
+			add(boton2);
+			boton2.addActionListener(this);
 		}//aqui termina el constructor
 
 			public void actionPerformed(ActionEvent e){
 				if(e.getSource() == boton1){
 					setTitle("Hola, oprimiste el boton");
+				}
+
+				if(e.getSource() == boton2){
+				System.exit(0);
 				}
 			}
 
